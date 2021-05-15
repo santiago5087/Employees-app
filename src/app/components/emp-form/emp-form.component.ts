dc32b37ef25818388e51c67d12a68c342bcdc73f
Santiago Marín <esmarina@unal.edu.co>
2021-05-15 13:17:30 -0500

    Improving view's look and feel

--- src/app/components/emp-form/emp-form.component.ts
+++ src/app/components/emp-form/emp-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common'
